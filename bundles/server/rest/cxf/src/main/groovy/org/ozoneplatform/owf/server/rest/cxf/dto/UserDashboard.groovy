8d5b7d8d995a2eea10abc25053ac4f4b3e22ecc2
msmolyak <michael.smolyak@nextcentury.com>
2013-01-14 10:33:48 -0500

    OZP-70 Prototyping Dashboard Controllers

--- bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/dto/UserDashboard.groovy
+++ bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/dto/UserDashboard.groovy
@@ -1,8 +1,52 @@
 package org.ozoneplatform.owf.server.rest.cxf.dto
 
-class UserDashboard extends Dashboard {
+import javax.xml.bind.annotation.XmlRootElement
 
-    UserDashboard(name, guid, isDefault, dashboardPosition, alteredByAdmin, user) {
-        super(name, guid, isDefault, dashboardPosition, alteredByAdmin)
+@XmlRootElement
+class UserDashboard {
+
+    String name
+    String guid
+    boolean isDefault //Capitalized d, should we change all booleans to is*?
+    Integer dashboardPosition
+    boolean alteredByAdmin
+    String description = ''
+    String layoutConfig = ''
+    boolean locked = false
+
+    public UserDashboard() {
+
+    }
+
+    public UserDashboard(name, guid, isDefault, dashboardPosition, alteredByAdmin) {
+        this.name = name
+        this.guid = guid
+        this.isDefault = isDefault
+        this.dashboardPosition = dashboardPosition
+        this.alteredByAdmin = alteredByAdmin
+    }
+
+    boolean getAlteredByAdmin() {
+        return alteredByAdmin
+    }
+
+    void setAlteredByAdmin(boolean alteredByAdmin) {
+        this.alteredByAdmin = alteredByAdmin
+    }
+
+    boolean getLocked() {
+        return locked
+    }
+
+    void setLocked(boolean locked) {
+        this.locked = locked
+    }
+
+    boolean getIsDefault() {
+        return isDefault
+    }
+
+    void setIsDefault(boolean aDefault) {
+        isDefault = aDefault
     }
 }
