c0e63c5baefe75dcf70d22f3926dd0c79b9a4cd1
Johnathan Gilday <johnathan.gilday@nextcentury.com>
2013-01-21 11:13:14 -0500

    OZP-70: Refactored REST controllers. Centralized Exception handling in JAX-RS Exception Mappers

--- bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/PersonController.groovy
+++ bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/PersonController.groovy
@@ -1,114 +1,75 @@
 package org.ozoneplatform.owf.server.rest.cxf
+import org.ozoneplatform.owf.server.service.PersonService
+import org.ozoneplatform.owf.server.service.model.Person
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import org.ozoneplatform.owf.server.service.PersonService;
-import org.ozoneplatform.owf.server.service.model.Person;
-import org.ozoneplatform.owf.server.service.exception.NotFoundException;
-import org.ozoneplatform.owf.server.service.exception.ValidationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.*
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
+import javax.ws.rs.core.UriInfo
 
 @Path("/persons")
 @Produces("application/json")
 class PersonController {
-    
+
     PersonService personService;
-    
+
     @Context
     private UriInfo uriInfo;
-    
+
     @GET
     Response list() {
-        try {
-            List<Person> list = personService.list();
-            if (list && !list.empty) {
-                Response.ok(list).build();
-            } else {
-                Response.noContent().build();
-            }
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
+        List<Person> list = personService.list();
+        if (list && !list.empty) {
+            Response.ok(list).build();
+        } else {
+            Response.noContent().build();
         }
     }
-    
+
     @POST
     @Consumes("application/json")
     Response create(Person person) {
-        try {
-            Person thePerson = personService.create(person);
-            URI personUri;
-            UriBuilder builder = uriInfo.getBaseUriBuilder();
-            builder.path(PersonController.class);
-            builder.path(PersonController.class.getMethod("fetch", Long.class));
-            personUri = builder.build(person.id);
-            Response.created(personUri).entity(thePerson).build();
-        } catch(ValidationException v) {
-            Response.status(400).entity(v.getMessage()).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Person thePerson = personService.create(person);
+        URI personUri;
+        UriBuilder builder = uriInfo.getBaseUriBuilder();
+        builder.path(PersonController.class);
+        builder.path(PersonController.class.getMethod("fetch", Long.class));
+        personUri = builder.build(person.id);
+        Response.created(personUri).entity(thePerson).build();
     }
-    
+
     @GET
     @Path("/{id}")
     Response fetch(@PathParam("id") Long id) {
-        try {
-            Person thePerson = personService.fetch(id);
-            Response.ok(thePerson).build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Person thePerson = personService.fetch(id);
+        Response.ok(thePerson).build();
     }
-    
+
     @PUT
     @Path("/{id}")
     @Consumes("application/json")
     Response update(@PathParam("id") Long id, Person person) {
-        try {
-            Response.ok(personService.update(id, person)).build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(ValidationException v) {
-            Response.status(400).entity(v.getMessage()).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Response.ok(personService.update(id, person)).build();
     }
-    
+
     @DELETE
     @Path("/{id}")
     Response delete(@PathParam("id") Long id) {
-        try {
-            personService.delete(id);
-            Response.ok().build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        personService.delete(id);
+        Response.ok().build();
     }
-    
+
     @GET
     @Path("/{id}/dashboards")
     Response listDashboards(@PathParam("id") Long id) {
         Response.ok().build();
     }
-    
+
     @GET
     @Path("/{id}/widgets")
     Response listWidgets(@PathParam("id") Long id) {
         Response.ok().build();
     }
-    
+
 }
--- bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/PreferenceController.groovy
+++ bundles/server/rest/cxf/src/main/groovy/org/ozoneplatform/owf/server/rest/cxf/PreferenceController.groovy
@@ -1,158 +1,99 @@
 package org.ozoneplatform.owf.server.rest.cxf
+import org.ozoneplatform.owf.server.service.PreferenceService
+import org.ozoneplatform.owf.server.service.model.Preference
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import org.ozoneplatform.owf.server.service.PreferenceService;
-import org.ozoneplatform.owf.server.service.model.Preference;
-import org.ozoneplatform.owf.server.service.exception.NotFoundException;
-import org.ozoneplatform.owf.server.service.exception.ValidationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.*
+import javax.ws.rs.core.Context
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.UriBuilder
+import javax.ws.rs.core.UriInfo
 
 @Path("/preferences")
 @Produces("application/json")
 class PreferenceController {
-    
+
     PreferenceService preferenceService;
-    
+
     @Context
     private UriInfo uriInfo;
 
     @GET
     Response list() {
-        try {
-            List<Preference> list = preferenceService.list();
-            if (list && !list.empty) {
-                Response.ok(list).build();
-            } else {
-                Response.noContent().build();
-            }
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
+        List<Preference> list = preferenceService.list();
+        if (list && !list.empty) {
+            Response.ok(list).build();
+        } else {
+            Response.noContent().build();
         }
     }
-    
+
     @GET
     @Path("/{namespace}")
     Response list(@PathParam("namespace") String namespace) {
-        try {
-            List<Preference> list = preferenceService.list(namespace);
-            if (list && !list.empty) {
-                Response.ok(list).build();
-            } else {
-                Response.noContent().build();
-            }
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
+        List<Preference> list = preferenceService.list(namespace);
+        if (list && !list.empty) {
+            Response.ok(list).build();
+        } else {
+            Response.noContent().build();
         }
     }
-    
+
     @DELETE
     @Path("/{namespace}")
     Response delete(@PathParam("namespace") String namespace) {
-        try {
-            preferenceService.delete(namespace);
-            Response.ok().build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        preferenceService.delete(namespace);
+        Response.ok().build();
     }
-    
+
     @GET
     @Path("/{namespace}/{path}")
     Response fetch(@PathParam("namespace") String namespace, @PathParam("path") String path) {
-        try {
-            Preference thePref = preferenceService.fetch(namespace, path);
-            Response.ok(thePref).build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Preference thePref = preferenceService.fetch(namespace, path);
+        Response.ok(thePref).build();
     }
-    
+
     @POST
     @Path("/{namespace}/{path}")
     @Consumes("application/json")
     Response create(@PathParam("namespace") String namespace, @PathParam("path") String path, Preference pref) {
-        try {
-            Preference thePref = preferenceService.create(namespace, path, pref);
-            URI prefUri;
-            UriBuilder builder = uriInfo.getBaseUriBuilder();
-            builder.path(PreferenceController.class);
-            builder.path(PreferenceController.class.getMethod("fetch", String.class, String.class));
-            prefUri = builder.build(pref.namespace, pref.path);
-            Response.created(prefUri).entity(thePref).build();
-        } catch(ValidationException v) {
-            Response.status(400).entity(v.getMessage()).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Preference thePref = preferenceService.create(namespace, path, pref);
+        URI prefUri;
+        UriBuilder builder = uriInfo.getBaseUriBuilder();
+        builder.path(PreferenceController.class);
+        builder.path(PreferenceController.class.getMethod("fetch", String.class, String.class));
+        prefUri = builder.build(pref.namespace, pref.path);
+        Response.created(prefUri).entity(thePref).build();
     }
-    
+
     @PUT
     @Path("/{namespace}/{path}")
     @Consumes("application/json")
     Response update(@PathParam("namespace") String namespace, @PathParam("path") String path, Preference pref) {
-        try {
-            Response.ok(preferenceService.update(namespace, path, pref)).build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(ValidationException v) {
-            Response.status(400).entity(v.getMessage()).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Response.ok(preferenceService.update(namespace, path, pref)).build();
     }
-    
+
     @DELETE
     @Path("/{namespace}/{path}")
     Response delete(@PathParam("namespace") String namespace, @PathParam("path") String path) {
-        try {
-            preferenceService.delete(namespace, path);
-            Response.ok().build();
-        } catch(NotFoundException f) {
-            Response.status(404).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        preferenceService.delete(namespace, path);
+        Response.ok().build();
     }
-    
+
     @GET
     @Path("/{namespace}/{path}/existence")
     Response doesPreferenceExist(@PathParam("namespace") String namespace, @PathParam("path") String path) {
-        try {
-            Response.ok(preferenceService.exists(namespace, path)).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Response.ok(preferenceService.exists(namespace, path)).build();
     }
-    
+
     @GET
     @Path("/server/resources")
     Response getServerResources(@PathParam("namespace") String namespace, @PathParam("path") String path) {
-        try {
-            Response.ok(["Resource One", "Resource Two"]).build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Response.ok(["Resource One", "Resource Two"]).build();
     }
-    
+
     @GET
     @Path("/server/who")
     Response whoami(@PathParam("namespace") String namespace, @PathParam("path") String path) {
-        try {
-            Response.ok("me").build();
-        } catch(Exception e) {
-            Response.serverError().entity(e.toString()).build();
-        }
+        Response.ok("me").build();
     }
 }
