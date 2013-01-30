93dc5f025ac23d43c4c0a71a32645a014dc4d1b1
Ross Pokorny <ross.pokorny@nextcentury.com>
2013-01-30 11:01:49 -0500

    OZP-54: Taskbar header auto-resize

--- bundles/client/dashboard/dev/src/main/webapp/js/views/Taskbar.js
+++ bundles/client/dashboard/dev/src/main/webapp/js/views/Taskbar.js
@@ -32,6 +32,10 @@ define([
             View.prototype.initialize.apply(this, arguments);
 
             this.collection = options.collection;
+
+            //TODO: This is temporary, take it out once dashboards
+            //have code to call pane resize
+            $(window).on('resize', _.bind(this.resize, this));
         },
 
         render: function() {
@@ -46,6 +50,44 @@ define([
 
             header.render();
             this.$el.append(header.$el);
+
+            this.handleOverflow();
+        },
+
+        resize: function() {
+            this.handleOverflow();
+        },
+
+        handleOverflow: function() {
+            var taskbarWidth = this.$el.width(),
+                unchangeableWidth = 0,  //the total width of paddings, borders, and margins on content
+                changeableWidth = 0,    //sum of inner widths of headers
+                ratio;
+
+            this.$el.children('.header').each(function(idx, header) {
+                var $header = $(header),
+                    outerWidth,
+                    innerWidth;
+
+                $(header).css('width', ''); //unset manual width
+                outerWidth = $header.outerWidth(true); //true to include margin
+                innerWidth = $header.width();
+
+                changeableWidth += innerWidth;
+                unchangeableWidth += outerWidth - innerWidth;
+            });
+
+            //need taskbarWidth = unchangeableWidth + changeableWidth * ratio; solve for ratio
+            ratio = (taskbarWidth - unchangeableWidth) / changeableWidth;
+
+            //if content is too wide, resize according to calculated ratio
+            if (ratio < 1) {
+                this.$el.children('.header').each(function(idx, header) {
+                    var $header = $(header);
+
+                    $header.width($header.width() * ratio);
+                });
+            }
         }
     });
 });
